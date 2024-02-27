6dd1a637cf95afd5c10ff811b22da389103c3075
DanDan-Dan <148914957+DanDan-Dan@users.noreply.github.com>
2024-02-27 15:15:08 -0500

    added a code for conveyer
    needed some more editing

--- src/main/java/frc/robot/subsystems/conveyorSubsystem.java
+++ src/main/java/frc/robot/subsystems/conveyorSubsystem.java
@@ -4,5 +4,45 @@
 
 package frc.robot.subsystems;
 
-/** Add your docs here. */
-public class conveyorSubsystem {}
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkFlex;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.SubsystemBase; 
+
+
+public class conveyorSubsystem extends SubsystemBase {
+ private CANSparkFlex topconveyor;
+ private CANSparkFlex bottomconveyor;
+
+ public DigitalInput conveyerlimitswitch;
+ private double topconveyorshooter= 3;
+ private double bottomconveyorshooter= 3;
+
+public conveyorSubsystem (){
+    topconveyor = new CANSparkFlex(14, MotorType.kBrushless);
+    topconveyor = .setInverted(false);
+    topconveyor = .setIdleMode( IdleMode.kCoast);
+    topconveyor.burnFlash();
+
+    bottomconveyor = new CANSparkFlex(15 , MotorType.kBrushless);
+    bottomconveyor = .setInverted(true);
+    bottomconveyor = .setIdleMode( IdleMode.kCoast);
+    bottomconveyor.burnFlash();
+
+    conveyerlimitswitch= new DigitalInput(4);
+} 
+private void conveyerMotorSpeed( double motorSpeedPercentage){
+    double conveyerSpeed= motorSpeedPercentage/100;
+    topconveyor.set(conveyerSpeed);
+    bottomconveyor.set(conveyerSpeed);
+}
+
+public void stopConveyer (){
+    topconveyor.set(0);
+    bottomconveyor.set(0); 
+
+     }
+}
+
+
